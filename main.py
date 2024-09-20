f12659ba2f610cf70add77f9f6be27e44f1eab25
Paulo Haliski <paulohaliski50@gmail.com>
2024-09-19 21:22:59 -0300

    fazendo o acesso à elevor

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ import pyautogui
 driver = webdriver.Firefox()
 
 driver.get('https://mauer.2cloud.com.br/ERP360/Login.aspx')
+driver.maximize_window()
 
-sleep(10)
+sleep(1)
 
 login = driver.find_element(By.ID, "ContentPlaceHolder1_txtLogin")
 senha = driver.find_element(By.ID, "ContentPlaceHolder1_txtSenha")
@@ -16,25 +17,31 @@ botao = driver.find_element(By.ID, "ContentPlaceHolder1_btnLogin")
 
 login.send_keys("mauer.PauloAlexandre")
 senha.send_keys("ElevorPaulo2024@")
-sleep(0.2)
+sleep(0.1)
 botao.click()
 
 sleep(5)
-
-driver.maximize_window()
 
 pyautogui.moveTo(2, 500)
 
 sleep(1)
 
-barra_lateral = driver.find_element(By.ID, "ContentPlaceHolder1_asideMasterPage")
-# fixar_menu = driver.find_element(By.XPATH, "//*[contains(@class, 'fa') and contains(@class, 'fa-bars')]")
+botao_fixar_location = pyautogui.locateCenterOnScreen('botao_fixar.png', confidence=0.8)
+pyautogui.click(botao_fixar_location)
 
-# fixar_menu.click()
+botao_financeiro_location = pyautogui.locateCenterOnScreen('botao_financeiro.png', confidence=0.8)
+pyautogui.click(botao_financeiro_location)
+
+'''
+barra_lateral = driver.find_element(By.ID, "ContentPlaceHolder1_asideMasterPage")
+fixar_menu = driver.find_element(By.XPATH, "//*[contains(@class, 'fa') and contains(@class, 'fa-bars')]")
+
+fixar_menu.click()
+
 
 botao_financeiro = driver.find_element(By.CSS_SELECTOR, "em.far.fa-money-bill-alt")
 botao_financeiro.find_element(By.XPATH, "..").click()
 
 # botao_financeiro.click()
-
+'''
 # driver.quit()
