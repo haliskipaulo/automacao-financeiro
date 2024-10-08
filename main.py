6f75996fafbcaf1704cb3cab00dbeae7bbd762a5
Paulo Haliski <paulohaliski50@protonmail.com>
2024-10-08 10:31:11 -0300

    alterações

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 import pyautogui
+
+#   ABRIR A ELEVOR ABAIXO
 
 driver = webdriver.Firefox()
 
@@ -15,8 +19,8 @@ login = driver.find_element(By.ID, "ContentPlaceHolder1_txtLogin")
 senha = driver.find_element(By.ID, "ContentPlaceHolder1_txtSenha")
 botao = driver.find_element(By.ID, "ContentPlaceHolder1_btnLogin")
 
-login.send_keys("mauer.PauloAlexandre")
-senha.send_keys("ElevorPaulo2024@")
+login.send_keys("") # inserir login
+senha.send_keys("") # inserir senha
 sleep(0.1)
 botao.click()
 
@@ -26,10 +30,13 @@ pyautogui.moveTo(2, 500)
 
 sleep(1)
 
-'''
+barra_lateral = driver.find_element(By.ID, "ContentPlaceHolder1_asideMasterPage")
+fixar_menu  = driver.find_element(By.XPATH, "//aside[@id='ContentPlaceHolder1_asideMasterPage']//nav[@id='ContentPlaceHolder1_menuSidebar']//div[@class='profile-picture']//a[@class='fixed-sidebar float-right d-none d-md-inline']")
 
-botao_fixar_location = pyautogui.locateCenterOnScreen('botao_fixar.png', confidence=0.8)
-pyautogui.click(botao_fixar_location)
+fixar_menu.click()
+
+# botao_fixar_location = pyautogui.locateCenterOnScreen('botao_fixar.png', confidence=0.8)
+# pyautogui.click(botao_fixar_location)
 
 botao_financeiro_location = pyautogui.locateCenterOnScreen('botao_financeiro.png', confidence=0.8)
 sleep(0.2)
@@ -39,20 +46,8 @@ botao_bancosCaixa_location = pyautogui.locateCenterOnScreen('botao-bancoscaixa.p
 sleep(0.2)
 pyautogui.click(botao_bancosCaixa_location)
 
-PYAUTOGUI FUNCIONANDO
-
-'''
-
-barra_lateral = driver.find_element(By.ID, "ContentPlaceHolder1_asideMasterPage")
-fixar_menu  = driver.find_element(By.XPATH, "//aside[@id='ContentPlaceHolder1_asideMasterPage']//nav[@id='ContentPlaceHolder1_menuSidebar']//div[@class='profile-picture']//a[@class='fixed-sidebar float-right d-none d-md-inline']")
-
-fixar_menu.click()
-
 sleep(1)
 
-botao_financeiro = driver.find_element(By.CLASS_NAME, "far fa-money-bill-alt")
-
-
-# botao_financeiro.click()
-
 # driver.quit()
+
+# ABRIR ELEVOR ACIMA
