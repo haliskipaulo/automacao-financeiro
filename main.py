52c43721af8bcc13367591091efe49367943796b
Paulo Haliski <paulohaliski50@protonmail.com>
2024-10-10 11:25:05 -0300

    commit

--- main.py
+++ main.py
@@ -13,45 +13,46 @@ import shutil
 
 #   ABRIR A ELEVOR ABAIXO
 
-driver = webdriver.Firefox()
+def elevor():
+    driver = webdriver.Firefox()
 
-driver.get('https://mauer.2cloud.com.br/ERP360/Login.aspx')
-driver.maximize_window()
+    driver.get('https://mauer.2cloud.com.br/ERP360/Login.aspx')
+    driver.maximize_window()
 
-sleep(1)
+    sleep(1)
 
-login = driver.find_element(By.ID, "ContentPlaceHolder1_txtLogin")
-senha = driver.find_element(By.ID, "ContentPlaceHolder1_txtSenha")
-botao = driver.find_element(By.ID, "ContentPlaceHolder1_btnLogin")
+    login = driver.find_element(By.ID, "ContentPlaceHolder1_txtLogin")
+    senha = driver.find_element(By.ID, "ContentPlaceHolder1_txtSenha")
+    botao = driver.find_element(By.ID, "ContentPlaceHolder1_btnLogin")
 
-login.send_keys("") # inserir login
-senha.send_keys("") # inserir senha
-sleep(0.1)
-botao.click()
+    login.send_keys("") # inserir login
+    senha.send_keys("") # inserir senha
+    sleep(0.1)
+    botao.click()
 
-sleep(5)
+    sleep(5)
 
-pyautogui.moveTo(2, 500)
+    pyautogui.moveTo(2, 500)
 
-sleep(1)
+    sleep(1)
 
-barra_lateral = driver.find_element(By.ID, "ContentPlaceHolder1_asideMasterPage")
-fixar_menu  = driver.find_element(By.XPATH, "//aside[@id='ContentPlaceHolder1_asideMasterPage']//nav[@id='ContentPlaceHolder1_menuSidebar']//div[@class='profile-picture']//a[@class='fixed-sidebar float-right d-none d-md-inline']")
+    barra_lateral = driver.find_element(By.ID, "ContentPlaceHolder1_asideMasterPage")
+    fixar_menu  = driver.find_element(By.XPATH, "//aside[@id='ContentPlaceHolder1_asideMasterPage']//nav[@id='ContentPlaceHolder1_menuSidebar']//div[@class='profile-picture']//a[@class='fixed-sidebar float-right d-none d-md-inline']")
 
-fixar_menu.click()
+    fixar_menu.click()
 
-# botao_fixar_location = pyautogui.locateCenterOnScreen('botao_fixar.png', confidence=0.8)
-# pyautogui.click(botao_fixar_location)
+    # botao_fixar_location = pyautogui.locateCenterOnScreen('botao_fixar.png', confidence=0.8)
+    # pyautogui.click(botao_fixar_location)
 
-botao_financeiro_location = pyautogui.locateCenterOnScreen('botao_financeiro.png', confidence=0.8)
-sleep(0.2)
-pyautogui.click(botao_financeiro_location)
+    botao_financeiro_location = pyautogui.locateCenterOnScreen('botao_financeiro.png', confidence=0.8)
+    sleep(0.2)
+    pyautogui.click(botao_financeiro_location)
 
-botao_bancosCaixa_location = pyautogui.locateCenterOnScreen('botao-bancoscaixa.png', confidence=0.8)
-sleep(0.2)
-pyautogui.click(botao_bancosCaixa_location)
+    botao_bancosCaixa_location = pyautogui.locateCenterOnScreen('botao-bancoscaixa.png', confidence=0.8)
+    sleep(0.2)
+    pyautogui.click(botao_bancosCaixa_location)
 
-sleep(1)
+    sleep(1)
 
 # driver.quit()
 
